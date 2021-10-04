637864905532516cdaf4218ae250fbbbe0d17790
Greg Mozer <mozer.grzegorz@yahoo.com>
2021-10-03 23:38:41 -0400

    update import to alias

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
-import Layout from "../components/Layout";
+import Layout from "@/components/Layout";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/globals.css";
+import "@/styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
