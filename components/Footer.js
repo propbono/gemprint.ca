be9b659dbab5399cf8ddb51e5fddea1c7509c6b1
Greg Mozer <mozer.grzegorz@yahoo.com>
2021-11-08 22:29:04 -0500

    prepare footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,38 +1,42 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Nav from "react-bootstrap/Nav";
 
-import footerStyles from "@/styles/Footer.module.scss";
+
+const styles = {
+  footerSection: "bg-secondary text-gray-100 text-lg",
+  footerWrapper: "flex text-center md:text-left flex-col-reverse w-full md:w-auto md:justify-between md:flex-row",
+  socialLinks:
+    "hover:text-primary md:before:content-['/'] md:before:opacity-40 md:before:mr-4 text-decoration-none my-2 md:my-0 md:ml-4",
+  socialMenu:
+    "flex flex-col md:flex-row items-center md:justify-end mb-3 md:mb-0",
+};
+
 
 export const Footer = () => {
   return (
-    <footer className={footerStyles.footer}>
-      Footer
-      {/* <Container>
-        <Row className="d-flex justify-content-between">
-          <Col sm={6}>
+    <footer className={styles.footerSection}>
+      <div className="container py-6">
+        <div className={styles.footerWrapper}>
+          <div>
             <p>© 2017-2021 Gemprint</p>
-          </Col>
-          <Col sm={6} className="d-flex justify-content-end">
-            <Nav>
-              <li className={footerStyles.socialLinks}>
+          </div>
+          <div>
+            <ul className={styles.socialMenu}>
+              <li className={styles.socialLinks}>
                 <a href="https://www.facebook.com/GemprintGTA">Facebook</a>
               </li>
-              <li className={footerStyles.socialLinks}>
+              <li className={styles.socialLinks}>
                 <a href="https://www.instagram.com/gemprint_gta/">Instagram</a>
               </li>
-              <li className={footerStyles.socialLinks}>
+              <li className={styles.socialLinks}>
                 <a href="https://twitter.com/GemprintTheSix">Twitter</a>
               </li>
-              <li className={footerStyles.socialLinks}>
+              <li className={styles.socialLinks}>
                 <a href="https://linkedin.com">LinkedIn</a>
               </li>
-            </Nav>
-          </Col>
-        </Row>
-      </Container> */}
+            </ul>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
