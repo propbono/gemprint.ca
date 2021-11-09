649a0a1b28bf6126eb707399b10bd84e8e8072cf
Greg Mozer <mozer.grzegorz@yahoo.com>
2021-11-08 22:28:55 -0500

    update styles

--- components/Hero.js
+++ components/Hero.js
@@ -1,47 +1,37 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Image from "next/image";
-
-import heroStyles from "@/styles/Hero.module.scss";
 
 export const Hero = () => {
   return (
-    <section className={heroStyles.hero}>
-      <Container>
-        <Row>
-          <Col xs={9} sm={11} lg={9} className={`card ${heroStyles.heroCard}`}>
-            <div className="card-body">
-              <p>
-                May 2, 2018 by{" "}
-                <strong className="text-primary">Maimuna Gabeyre</strong>
-              </p>
-              <h2 className={heroStyles.heroCardTitle}>
-                Be original, be creative choose Gemprint
-              </h2>
-              <Image
-                src="/images/zigzag.svg"
-                className={heroStyles.heroCardSeparator}
-                alt="zig_zag image"
-                width={100}
-                height={20}
-              />
-              <p className={heroStyles.heroCardContent}>
-                Are you looking for a reliable and affordable printer? We are
-                experts in print. Our experienced team is here to help you
-                succeed.
-              </p>
-              <a
-                href="mailto:maimuna.gabeyre@gemprint.ca"
-                className="mt-auto text-white btn btn-primary"
-              >
-                Request a Quote
-              </a>
-            </div>
-          </Col>
-        </Row>
-      </Container>
+    <section className="w-full h-auto bg-center bg-cover md:h-screen md:pt-24 bg-hero-section">
+      <div className="container py-6 md:py-24">
+        <div className="w-full px-8 pt-6 pb-10 bg-white rounded-md shadow-md md:opacity-60 md:w-3/4 hover:opacity-100">
+          <p className="mb-2 text-2xl">
+            May 2, 2018 by{" "}
+            <strong className="text-primary">Maimuna Gabeyre</strong>
+          </p>
+          <h2 className="mb-4 text-5xl font-bold text-secondary">
+            Be original, be creative choose Gemprint
+          </h2>
+          <Image
+            src="/images/zigzag.svg"
+            className="mb-4"
+            alt="zig_zag image"
+            width={100}
+            height={20}
+          />
+          <p className="mb-10 text-3xl">
+            Are you looking for a reliable and affordable printer? We are
+            experts in print. Our experienced team is here to help you succeed.
+          </p>
+          <a
+            href="mailto:maimuna.gabeyre@gemprint.ca"
+            className="px-6 py-4 my-auto text-xl text-white rounded bg-primary hover:bg-secondary hover:text-gray-100"
+          >
+            Request a Quote
+          </a>
+        </div>
+      </div>
     </section>
   );
 };
