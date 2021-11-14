f79efce5ed8dc0a7d2714c6389d548923b1ab7b3
Greg Mozer <mozer.grzegorz@yahoo.com>
2021-11-13 20:20:07 -0500

    refactor styles tp use object

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -1,5 +1,20 @@
 import React, { useState, useRef } from "react";
 
+const styles = {
+  section: "relative lg:top-[-12rem]",
+  container: "container",
+  newsletter:
+    "flex flex-col my-10 overflow-hidden transition duration-500 ease-in-out bg-white rounded-lg shadow-lg lg:h-56 lg:flex-row group lg:hover:scale-105",
+  subscribe: "items-center justify-center lg:flex lg:w-1/3 lg:bg-secondary",
+  title: "text-2xl font-bold text-secondary lg:text-gray-100",
+  text: "mt-2 text-secondary lg:text-gray-400",
+  input:
+    "flex flex-col items-center justify-center w-full pb-6 transition duration-1000 ease-in-out lg:py-0 lg:w-2/3 lg:border-b-8 border-secondary lg:group-hover:border-primary",
+  form: "flex flex-col overflow-hidden rounded-lg sm:flex-row",
+  email:
+    "flex flex-1 px-4 py-3 text-gray-800 placeholder-gray-500 bg-gray-200 border-2 border-gray-300 outline-none focus:bg-gray-100",
+};
+
 export const Subscribe = () => {
   const inputRef = useRef(null);
   const [message, setMessage] = useState("");
@@ -22,36 +37,36 @@ export const Subscribe = () => {
   };
 
   return (
-    <section className="relative lg:top-[-12rem]">
-      <div className="container">
-        <div className="flex flex-col my-10 overflow-hidden transition duration-500 ease-in-out bg-white rounded-lg shadow-lg lg:h-56 lg:flex-row group lg:hover:scale-105">
-          <div className="items-center justify-center lg:flex lg:w-1/3 lg:bg-secondary">
+    <section className={styles.section}>
+      <div className={styles.container}>
+        <div className={styles.newsletter}>
+          <div className={styles.subscribe}>
             <div className="px-8 py-6 lg:py-0">
-              <h2 className="text-2xl font-bold text-secondary lg:text-gray-100">
+              <h2 className={styles.title}>
                 Subscribe to our newsletter
               </h2>
-              <p className="mt-2 text-secondary lg:text-gray-400">
+              <p className={styles.text}>
                 We will only send emails once a month to update you on our
                 offer. No Spam!
               </p>
             </div>
           </div>
-          <div className="flex flex-col items-center justify-center w-full pb-6 transition duration-1000 ease-in-out lg:py-0 lg:w-2/3 lg:border-b-8 border-secondary lg:group-hover:border-primary">
-            <div className="w-[600px]">
+          <div className={styles.input}>
+            <div className="lg:w-[600px]">
               <form
                 onSubmit={subscribe}
-                className="flex flex-col overflow-hidden rounded-lg sm:flex-row"
+                className={styles.form}
               >
                 <input
                   ref={inputRef}
                   type="email"
                   placeholder="Enter your email"
                   id="email-newsletter"
-                  className="flex flex-1 max-w-6xl px-4 py-3 text-gray-800 placeholder-gray-500 bg-gray-200 border-2 border-gray-300 outline-none focus:bg-gray-100"
+                  className={styles.email}
                 />
                 <button
                   type="submit"
-                  className="flex-none btn btn-secondary"
+                  className="btn btn-secondary"
                 >
                   Subscribe
                 </button>
