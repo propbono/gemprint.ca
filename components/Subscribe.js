0444997acd9128781524f5ae4267ed748973e7ed
Greg Mozer <mozer.grzegorz@yahoo.com>
2021-11-14 22:59:52 -0500

    Optimize Subscribe component

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -1,4 +1,5 @@
 import React, { useState, useRef } from "react";
+import axios from "axios";
 
 const styles = {
   section: "relative lg:top-[-12rem]",
@@ -18,22 +19,25 @@ const styles = {
 export const Subscribe = () => {
   const inputRef = useRef(null);
   const [message, setMessage] = useState("");
-
+  const [state, setState] = useState("IDLE");
+  const messageClass = state === "SUCCESS" ? "text-green-600 mt-1" : "text-red-600 mt-1";
+  const isLoading = state === "LOADING" ? "animate-bounce" : "";
+  
   const subscribe = async (e) => {
     e.preventDefault();
-    const res = await fetch("/api/subscribe", {
-      body: JSON.stringify({ email: inputRef.current.value }),
-      headers: { "Content-Type": "application/json" },
-      method: "POST",
-    });
-    const { error } = await res.json();
-
-    if (error) {
-      setMessage(error);
-      return;
+    setState("LOADING");
+    setMessage("");
+    try {
+      const response = await axios.post("/api/subscribe", {email: inputRef.current.value});
+      console.log("Response: ", response);
+      setState("SUCCESS");
+      setMessage("Success! 🎉 You are now subscribed to the newsletter.");
+      inputRef.current.value = "";
+    } catch (error) {
+      console.log("Error: ", error)
+      setState("ERROR");
+      setMessage(error.response.data.message);
     }
-    inputRef.current.value = "";
-    setMessage("Success! 🎉 You are now subscribed to the newsletter.");
   };
 
   return (
@@ -42,9 +46,7 @@ export const Subscribe = () => {
         <div className={styles.newsletter}>
           <div className={styles.subscribe}>
             <div className="px-8 py-6 lg:py-0">
-              <h2 className={styles.title}>
-                Subscribe to our newsletter
-              </h2>
+              <h2 className={styles.title}>Subscribe to our newsletter</h2>
               <p className={styles.text}>
                 We will only send emails once a month to update you on our
                 offer. No Spam!
@@ -53,10 +55,7 @@ export const Subscribe = () => {
           </div>
           <div className={styles.input}>
             <div className="lg:w-[600px]">
-              <form
-                onSubmit={subscribe}
-                className={styles.form}
-              >
+              <form onSubmit={subscribe} className={styles.form}>
                 <input
                   ref={inputRef}
                   type="email"
@@ -64,15 +63,30 @@ export const Subscribe = () => {
                   id="email-newsletter"
                   className={styles.email}
                 />
+
                 <button
                   type="submit"
-                  className="btn btn-secondary"
+                  className="flex items-center justify-center btn btn-secondary"
                 >
                   Subscribe
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className={`w-6 h-6 mt-1 ml-2 ${isLoading}`}
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                    />
+                  </svg>
                 </button>
               </form>
             </div>
-            <div className="text-red-600">{message}</div>
+            <div className={messageClass}>{message}</div>
           </div>
         </div>
       </div>
