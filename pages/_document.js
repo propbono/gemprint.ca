421faec169852e191b1942a035e841519b8246e3
Greg Mozer <mozer.grzegorz@yahoo.com>
2021-12-05 10:12:05 -0500

    convert to arrow and remove getInitialProps

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,9 @@
-/* eslint-disable @next/next/no-document-import-in-page */
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-
+export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head />
         <body className="bg-off-white">
           <Main />
@@ -19,5 +13,3 @@ class MyDocument extends Document {
     );
   }
 }
-
-export default MyDocument;
