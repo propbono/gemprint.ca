308630604fda2d67069efa6cfac93f63f831c0f2
Greg Mozer <mozer.grzegorz@yahoo.com>
2021-12-07 01:44:06 -0500

    add HeroPost component

--- pages/blog.js
+++ pages/blog.js
@@ -1,29 +1,37 @@
 import React from "react";
 import { Link } from "next/link";
-import { Layout } from "@/components";
+import { Layout, HeroPost } from "@/components";
 import { getAllPosts } from "@/helpers";
 
 const Blog = ({ allPosts }) => {
-  //   console.log("Posts: ", allPosts);
   const heroPost = allPosts[0];
+  console.log("HeroLink", heroPost);
   const title = heroPost.title;
-  console.log("HeroPost: ", title);
   const posts = allPosts.slice(1);
 
   return (
     <Layout>
       <section className="min-h-screen py-24">
-        <h1>Blog</h1>
-        {posts.map(({ title, slug, date }) => (
-          <div key={slug} className="mb-8 bg-gray-300 shadow-lg rounder-md">
-            {/* {console.log("Title: ", title)} */}
-            <p>{title}</p>
-            {/* <Link href={`/blog/${heroPost.slug}`}>
-            <a className="text-3xl font-bold">{heroPost.title}</a>
-          </Link> */}
-            <p>{date}</p>
-          </div>
-        ))}
+        <div className="container px-4 mx-auto">
+          <h1>Blog</h1>
+          <HeroPost
+            title={heroPost.title}
+            author={heroPost.author}
+            date={heroPost.date}
+            excerpt={heroPost.excerpt}
+            link={heroPost.slug}
+            coverImage={heroPost.coverImage}
+          />
+          {/* {posts.map(({ title, slug, date }) => (
+            <div key={slug} className="mb-8 bg-gray-300 shadow-lg rounder-md">
+              <p>{title}</p>
+              <Link href={`/blog/${heroPost.slug}`}>
+                <a className="text-3xl font-bold">{heroPost.title}</a>
+              </Link>
+              <p>{date}</p>
+            </div>
+          ))} */}
+        </div>
       </section>
     </Layout>
   );
@@ -35,6 +43,7 @@ export const getStaticProps = async () => {
   const allPosts = getAllPosts([
     "title",
     "title",
+    "excerpt",
     "date",
     "slug",
     "author",
