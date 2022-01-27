8994ea702f5fe1ed883a2ba9755f7f15fac73b1b
Greg Mozer <mozer.grzegorz@yahoo.com>
2022-01-26 22:49:44 -0500

    fix corsOrigin="true"

--- pages/_document.js
+++ pages/_document.js
@@ -24,7 +24,11 @@ export default class MyDocument extends Document {
             href="/favicon-16x16.png"
           />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap"
             rel="stylesheet"
