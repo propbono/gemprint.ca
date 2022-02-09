2e3e78a814b682b03a0502db019fff3a34d48a1a
Greg Mozer <mozer.grzegorz@yahoo.com>
2022-02-08 21:15:06 -0500

    Update the logic for displaying posts

--- components/Post.js
+++ components/Post.js
@@ -3,27 +3,31 @@ import Image from "next/image";
 import { Tags } from "@/components";
 
 export const Post = ({ post }) => {
+  const {
+    content,
+    meta: { coverImage, altImage, date, tags, title, author },
+  } = post;
+
   return (
     <article className="relative flex flex-col mx-auto prose-xl prose-a:text-primary hover:prose-a:text-secondary dark:prose-invert prose-gray max-w-7xl">
       <div className="relative object-cover w-full hidden sm:block sm:h-[250px] md:h-[300px] lg:h-[600px] aspect-w-4 aspect-h-3">
         <Image
           layout="fill"
-          src={post.coverImage}
-          alt=""
+          src={coverImage}
+          alt={altImage}
           className="rounded-xl hover:opacity-75"
         />
       </div>
       <div className="flex flex-col justify-start p-6">
         <div className="mb-4 text-center ">
-          <Tags tags={post.tags} />
-          <h1 className="my-4 text-5xl font-bold text-primary">{post.title}</h1>
+          <Tags tags={tags} />
+          <h1 className="my-4 text-5xl font-bold text-primary">{title}</h1>
           <p className="mb-8 text-sm">
-            By{" "}
-            <strong className="font-bold text-primary ">{post.author}</strong>,
-            published on <strong>{post.date}</strong>
+            By <strong className="font-bold text-primary ">{author}</strong>,
+            published on <strong>{date}</strong>
           </p>
         </div>
-        <div dangerouslySetInnerHTML={{ __html: post.content }}></div>
+        <div dangerouslySetInnerHTML={{ __html: content }}></div>
       </div>
     </article>
   );
--- helpers/PostsApi.js
+++ helpers/PostsApi.js
@@ -4,17 +4,12 @@ import matter from "gray-matter";
 
 const postsDirectory = join(process.cwd(), "content/posts");
 
-/**
- * Reading the contents of the posts directory and returning an array of the file names.
- */
 export function getPostFiles() {
   return fs.readdirSync(postsDirectory);
 }
 
-/**
- * Get the post by slug and return the fields we want.
- */
 export function getPostBySlug(slug) {
+  console.log("Slug: ", slug);
   const realSlug = slug.replace(/\.mdx$/, "");
   const fullPath = join(postsDirectory, `${realSlug}.mdx`);
   const fileContents = fs.readFileSync(fullPath, "utf8");
@@ -23,18 +18,12 @@ export function getPostBySlug(slug) {
   return { slug: realSlug, meta: data, content };
 }
 
-/**
- * `getAllPosts` returns an array of all posts in the site.
- */
-export function getAllPosts(fields = []) {
+export function getAllPosts() {
   const slugs = getPostFiles();
   console.log("Slugs:", slugs);
-  return (
-    slugs
-      .map((slug) => getPostBySlug(slugs))
-      // sort posts by date in descending order
-      .sort((post1, post2) =>
-        new Date(post1.date) > new Date(post2.date) ? -1 : 1
-      )
-  );
+  return slugs
+    .map((slug) => getPostBySlug(slug))
+    .sort((post1, post2) =>
+      new Date(post1.date) > new Date(post2.date) ? -1 : 1
+    );
 }
