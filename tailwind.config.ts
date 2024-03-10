1f8aa12c1f62de0a262275aab3c8aa37f97a4776
G M <propbono@gmail.com>
2024-03-10 12:46:56 -0400

    chore: base app config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,14 +7,39 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        default: "2rem",
+        xs: "1rem",
+        sm: "2rem",
+        lg: "0rem",
+      },
+    },
     extend: {
+      fontFamily: {
+        sans: ["Ubuntu", "sans-serif"],
+      },
+      colors: {
+        primary: "#00afde",
+        secondary: "#354a54",
+        "secondary-dark": "#0d222b",
+        "secondary-light": "#607680",
+        "primary-light": "#63e1ff",
+        "primary-dark": "#0080ac",
+        "off-white": "#ECF0F1",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "about-section": "url('/images/About.webp')",
+        "hero-section": "url('/images/Hero.webp')",
+        "parallax-section": "url('/images/Parallax.webp')",
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
 export default config;
+
