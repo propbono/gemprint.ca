53f754dcd3c12c98d4b2822f57e715ba434267cc
G M <propbono@gmail.com>
2024-03-10 14:25:50 -0400

    feat: add footer links constant

---
+++ src/utils/constants.ts
@@ -0,0 +1,18 @@
+export const FOOTER_LINKS = [
+  {
+    title: "Facebook",
+    href: "https://www.facebook.com/GemprintGTA",
+  },
+  {
+    title: "Instagram",
+    href: "https://www.instagram.com/gemprint_gta/",
+  },
+  {
+    title: "Twitter",
+    href: "https://twitter.com/GemprintTheSix",
+  },
+  {
+    title: "LinkedIn",
+    href: "https://linkedin.com",
+  },
+];
