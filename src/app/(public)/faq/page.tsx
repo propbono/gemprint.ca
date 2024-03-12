63badffb97e4d0cabf2b22ba85796ccfcf70db55
G M <propbono@gmail.com>
2024-03-12 00:04:44 -0400

    feat: update accordion page

--- src/app/(public)/faq/page.tsx
+++ src/app/(public)/faq/page.tsx
@@ -10,11 +10,14 @@ import { FAQ } from "@/utils/constants";
 export default function Faq() {
   return (
     <Container>
-      <Accordion type="multiple" className="mt-6">
+      <h2 className="mt-6 text-center text-xl font-bold">
+        Frequently Asked Questions
+      </h2>
+      <Accordion type="multiple" className="mt-4">
         {FAQ.map(({ title, text }, index) => (
           <AccordionItem key={title} value={title} className="w-full">
-            <AccordionTrigger>{title}</AccordionTrigger>
-            <AccordionContent>{text}</AccordionContent>
+            <AccordionTrigger className="p-4">{title}</AccordionTrigger>
+            <AccordionContent className="px-8 py-4">{text}</AccordionContent>
           </AccordionItem>
         ))}
       </Accordion>
