0d19461df24656ba22650e1ae8a09d6003c5de77
G M <propbono@gmail.com>
2024-03-11 22:32:47 -0400

    refactor: refactor header to use drawer for mobile menu

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,27 +3,16 @@
 import { cn } from "@/utils/cn";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
-import { Button } from "../ui/button";
-import { HamburgerMenu } from "./hamburger-menu";
+import { MainMenu } from "./main-menu";
+import type { MenuItems } from "./types";
 
 // TODO: add framer motion animations.
 
-type HeaderProps = {
-  menuItems: {
-    title: string;
-    href: string;
-    asButton?: boolean;
-  }[];
-};
+type HeaderProps = MenuItems;
 
 export const Header = ({ menuItems }: HeaderProps) => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [headerClass, setHeaderClass] = useState("bg-opacity-75");
-  const pathname = usePathname();
-
-  console.log("PATHNAME: ", pathname);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -59,49 +48,7 @@ export const Header = ({ menuItems }: HeaderProps) => {
             height={40}
           />
         </Link>
-        <HamburgerMenu onChange={() => setIsMenuOpen(!isMenuOpen)} />
-        <div
-          className={cn(
-            "flex flex-row items-center justify-center gap-4",
-            isMenuOpen ? "block" : "hidden lg:flex"
-          )}
-        >
-          {menuItems &&
-            menuItems.map((menuItem) => {
-              const isActive = menuItem.href === pathname;
-
-              if (menuItem.asButton) {
-                return (
-                  <Button asChild key={menuItem.href} variant="default"
-                  className={cn("text-xl font-bold py-6",{
-                    "bg-primary text-secondary-foreground": isActive,
-                  })}
-                  >
-                    <Link
-                      href={menuItem.href}
-                    >
-                      {menuItem.title}
-                    </Link>
-                  </Button>
-                );
-              }
-
-              return (
-                <Link
-                  href={menuItem.href}
-                  key={menuItem.href}
-                  className={cn(
-                    "mr-6 mt-4 block text-xl font-bold transition  duration-500 ease-in-out hover:text-primary lg:mt-0",
-                    {
-                      "text-primary": isActive,
-                    }
-                  )}
-                >
-                  {menuItem.title}
-                </Link>
-              );
-            })}
-        </div>
+        <MainMenu menuItems={menuItems} />
       </nav>
     </header>
   );
