712f7a503180327fc0a59f4d4e952809f457200a
G M <propbono@gmail.com>
2024-03-11 23:43:50 -0400

    fix: update the padding in main

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <>
       <Header menuItems={HEADER_MENU_ITEMS} />
-      <main className="flex flex-grow  flex-col items-center justify-between bg-pink-100 p-24">
+      <main className="flex flex-grow  flex-col items-center justify-between bg-pink-100 pt-24">
       {children}
       </main>
       <Footer links={FOOTER_LINKS} />
