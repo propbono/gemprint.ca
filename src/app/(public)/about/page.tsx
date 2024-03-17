d1b5a683a39643beafe0cded0ce166f00efc658f
G M <propbono@gmail.com>
2024-03-17 13:18:25 -0400

    feat: finish services section

--- src/app/(public)/about/page.tsx
+++ src/app/(public)/about/page.tsx
@@ -7,12 +7,13 @@ import aboutImage from "@/images/about.webp";
 import { LINK_CARD_ITEMS } from "@/utils/constants";
 import Image from "next/image";
 import Link from "next/link";
+import { LuArrowDownCircle } from "react-icons/lu";
 
 export default function About() {
   return (
     <>
-      <Section id="about">
-        <Container className="flex items-center justify-center gap-8">
+      <Section id="about" className="border-b-2 border-dashed border-b-primary">
+        <Container className="flex items-center justify-center gap-8 lg:pb-16 lg:pt-40">
           <div className="w-full md:w-1/2">
             <Heading as="h1">Experts in Print</Heading>
             <Heading as="h4">
@@ -53,16 +54,20 @@ export default function About() {
         </Container>
       </Section>
       <Section className=" bg-slate-50" id="services">
-        <Container className="space-y-6 px-4 py-8 md:py-12 lg:py-20">
+        <Container className="space-y-6 px-4 py-8 md:py-12 lg:py-12">
+          <div className="flex items-center justify-center text-primary">
+            <LuArrowDownCircle size={32} />
+          </div>
           <div className="mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center">
             <Heading as="h2">
               How we change <span className="text-primary">the game</span>.
             </Heading>
             {/* TODO: Change this paragraph verbiage */}
             <p className="max-w-[85%] leading-normal text-muted-foreground sm:text-lg sm:leading-7">
-              The product can personalize user experiences by understanding
-              individual preferences and tailoring recommendations or content
-              based on user behavior and historical data.
+              Elevate your print and web design experience with us. Uncover how
+              we`&apos;re transforming the industry standards. Let`&apos;s
+              revolutionize your brand`&apos;s presence with tailored designs
+              and personalized content that captivate your audience.
             </p>
           </div>
           <div className="mx-auto grid justify-center gap-4 sm:grid-cols-2 md:max-w-[64rem] md:grid-cols-3">
