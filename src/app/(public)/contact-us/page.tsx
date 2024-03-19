ed85ef08829bf3f52e5dbd45107f9a4d3449a696
G M <propbono@gmail.com>
2024-03-19 18:19:59 -0400

    fix: update colors for footer and contact form, change verbiage

--- src/app/(public)/contact-us/page.tsx
+++ src/app/(public)/contact-us/page.tsx
@@ -9,18 +9,27 @@ export default function ContactUs() {
     <Section className="h-full">
       <Container className="h-full pt-0 lg:p-0">
         <div className="flex h-full flex-col md:flex-row">
-          <div className="w-full flex-grow bg-foreground p-6 text-primary-foreground md:w-1/3 lg:pt-32">
+          <div className="w-full flex-grow bg-secondary-foreground p-6 text-primary-foreground md:w-1/3 lg:pt-32">
             <Heading as="h1" className="text-left text-primary-foreground">
               <span className="text-primary">Let&apos;s talk!</span> Tell us all{" "}
               <u>about your project</u>.
             </Heading>
             <p className="mt-4 text-lg leading-relaxed text-muted-foreground">
-              Use this form to contact us if you would like to know more about
-              Gemprint, our services and products. Our standard response time to
-              this form communication is less than a day, in most cases much
-              faster.
+              Thank you for considering Gemprint for your inquiries about our
+              services and products. Please utilize this form to reach out to us
+              and discover more about what we offer.
             </p>
-
+            <p className="mt-4 text-lg leading-relaxed text-primary-foreground">
+              If you have specific printing needs, we encourage you to provide
+              all necessary details in your message to help us better understand
+              your requirements.
+            </p>
+            <p className="mt-4 text-lg leading-relaxed text-muted-foreground">
+              Rest assured, we strive to respond promptly to all submissions
+              through this form. Our standard response time is typically less
+              than a day, often much quicker, ensuring you receive the
+              assistance you need in a timely manner.
+            </p>
             <div className="mt-5 flex items-center gap-2 text-2xl font-bold">
               <LuPhoneCall className="text-primary" size={16} />
               <span className="text-sm">+1 416 666 1676</span>
